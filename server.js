0618bae909b1984b0f7f1d1c2ff28ad5b4448daf
Alalinga <alalinga.mubarik@amalitech.org>
2022-01-04 03:08:46 +0000

    made changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import _ from './env.js'
 import express from 'express';
 import bodyParser from 'body-parser';
 import envelopes from './routes/envelopes.js';
+import path from 'node:path';
 
 
 
@@ -9,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
+app.use(express.static('./public'));
 app.use(express.json())
 app.use(bodyParser.json());
 app.use(
